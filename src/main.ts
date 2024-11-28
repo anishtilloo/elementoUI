b856fb6543708d24bc5c7b47e5a08022436586cc
Anish Tilloo <anishtilloo@Anishs-MacBook-Air.local>
2024-11-29 00:35:22 +0530

    changed the name of the package and also imported the styles into the main entry file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Button1 from "./components/Button.vue"
 import HelloWorld1 from "./components/HelloWorld.vue";
 import Card from "./components/Card.vue";
 
+import "./style.css";
+
 export {
     Button1,
     HelloWorld1,
